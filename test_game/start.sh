5b3b67fb5c0d3a82e870be44c5cd12cbd5570ed3
Nicholas Ferguson <me@njf.io>
2025-06-11 16:49:50 -0400

    Delete test_game directory

--- test_game/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Quick start script for the 3D Roguelike game
-
-echo "Starting 3D Roguelike Dungeon Crawler..."
-echo "Opening http://localhost:8000 in your browser..."
-echo "Press Ctrl+C to stop the server"
-echo ""
-
-python3 server.py
